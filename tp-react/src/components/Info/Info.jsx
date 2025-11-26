e28274482f634a57752e0a0063f9edd97210282a
HongnamKim <ghdskawkd@naver.com>
2025-11-26 21:33:34 +0900

    feat: 평균점수 영역 토글 기능 추가
    - 평균점수(CPM, ACC, CNT) 영역 접기/펼치기 버튼 추가
    - 토글 상태 localStorage에 저장 (averagesVisible)
    - info 영역과 quote 영역 간격 조정

--- tp-react/src/components/Info/Info.jsx
+++ tp-react/src/components/Info/Info.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ThemeContext } from "../../Context/ThemeContext";
 import "./Info.css";
 import ResultPeriod from "./ResultPeriod/ResultPeriod";
@@ -6,10 +6,29 @@ import CurrentLastCpm from "./Cpms/CurrentLastCpm";
 import HighestCpm from "./Cpms/HighestCpm";
 import AverageScore from "./AverageScores/AverageScore";
 import { ScoreContext } from "../../Context/ScoreContext";
+import { Storage_Averages_Visible } from "../../const/config.const";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa6";
 
 const Info = () => {
   const { isDark } = useContext(ThemeContext);
   const { totalScore } = useContext(ScoreContext);
+
+  const [averagesVisible, setAveragesVisible] = useState(() => {
+    return localStorage.getItem(Storage_Averages_Visible) !== "false";
+  });
+
+  const toggleAverages = () => {
+    const newValue = !averagesVisible;
+    setAveragesVisible(newValue);
+    localStorage.setItem(Storage_Averages_Visible, newValue);
+  };
+
+  const getAveragesClassName = () => {
+    let className = "info-averages";
+    if (isDark) className += " dark";
+    if (!averagesVisible) className += " collapsed";
+    return className;
+  };
 
   return (
     <div className={isDark ? "info-background info-dark" : "info-background"}>
@@ -20,11 +39,19 @@ const Info = () => {
         <CurrentLastCpm />
         <HighestCpm />
       </div>
-      <div className={isDark ? "info-averages dark" : "info-averages"}>
+      <div className={getAveragesClassName()}>
         {Object.keys(totalScore).map(
           (value, index) =>
             value !== "highestCpm" && <AverageScore type={value} key={index} />,
         )}
+      </div>
+      <div className="averages-toggle-container">
+        <button
+          className={isDark ? "averages-toggle-btn dark" : "averages-toggle-btn"}
+          onClick={toggleAverages}
+        >
+          {averagesVisible ? <FaChevronUp /> : <FaChevronDown />}
+        </button>
       </div>
     </div>
   );
